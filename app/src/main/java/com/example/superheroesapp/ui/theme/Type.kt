39b8344a7c4037e6f05968d5ed4f50823430a686
Reuben <gathiireuben@gmail.com>
2023-08-25 15:17:40 +0300

    Added Cabin font family and Typography

--- app/src/main/java/com/example/superheroesapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/superheroesapp/ui/theme/Type.kt
@@ -2,16 +2,41 @@ package com.example.superheroesapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.superheroesapp.R
+
+
+val Cabin = FontFamily(
+    Font(R.font.cabin_regular, FontWeight.Normal),
+    Font(R.font.cabin_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    body1 = TextStyle( //bodyLarge
+        fontFamily = Cabin,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    subtitle1 = TextStyle( //displayLarge
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
+    ),
+    subtitle2 = TextStyle( // displayMedium
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    body2 = TextStyle( //displaySmall
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     )
     /* Other default text styles to override
     button = TextStyle(
