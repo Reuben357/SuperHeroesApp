a5140b6b56da4dec89bbb6977b7ecd286edc2f1c
Reuben <gathiireuben@gmail.com>
2023-08-25 15:18:59 +0300

    Added a top bar and called the HeroScreen heroes list function

--- app/src/main/java/com/example/superheroesapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroesapp/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.superheroesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheroesapp.model.HeroesRepo
 import com.example.superheroesapp.ui.theme.SuperHeroesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,14 +36,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HeroApp(){
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            HeroesTopBar()
+        }
+    ) {
+        val heroes = HeroesRepo.heroes
+        HeroesScreen().HeroesList(heroes = heroes)
+    }
 }
+
+@Composable
+fun HeroesTopBar(modifier: Modifier = Modifier){
+Row(
+    verticalAlignment = Alignment.CenterVertically,
+    modifier = modifier
+) {
+    Text(
+        text = stringResource(R.string.app_name),
+        style = MaterialTheme.typography.subtitle1
+
+    )
+}
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SuperHeroesAppTheme {
-        Greeting("Android")
+        HeroApp()
     }
 }
